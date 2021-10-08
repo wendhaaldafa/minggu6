9410d5d5d7c6cb8f14407fdeb89731cfc94d1451
wendhaaldafa <wendha4429xi2@gmail.com>
2021-10-08 21:50:09 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            // //prak2
+            // 'name' => 'Administrator',
+            // 'email' => 'admin@admin.com',
+            // 'password' => Hash::make('password'),
+
+            //prak3
+            'username' => 'admin',
+            'name' => 'Administrator Baru',
+            'email' => 'admin.baru@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+
+    }
+}
